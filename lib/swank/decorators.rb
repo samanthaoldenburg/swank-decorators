ff2bf35abde08fd252b7a43598e6e2de85bd70ae
Luna Oldenburg <luna@Voltaire.attlocal.net>
2024-12-23 09:58:35 -0600

    feat!: initial gem

---
+++ lib/swank/decorators.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "decorators/version"
+
+module Swank
+  module Decorators
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/swank/decorators_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Swank::Decorators do
+  it "has a version number" do
+    expect(Swank::Decorators::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
